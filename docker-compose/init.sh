345761c0acf28587f7c90302dcc5a3520fad0779
Desislav Arashev <desislav.arashev@daiteap.com>
2022-10-13 17:42:11 +0300

    Fix service logos not showing

--- docker-compose/init.sh
+++ docker-compose/init.sh
@@ -6,7 +6,6 @@ docker-compose up -d
 sleep 25
 
 docker exec daiteap-ui mkdir -p /app/cloudcluster_media
-docker cp docker-compose/service_logo daiteap-ui:/app/cloudcluster_media/
 docker exec daiteap-platform bash /docker-compose/platform-api-migrate.sh
 docker exec daiteap-platform python manage.py fix_service_catalog_prod
 docker exec daiteap-platform bash /docker-compose/platform-api-init-user.sh
