d97ed585b2d7c5bc873b5a7aacd7615d55322cf2
Viktoria Georgieva <viktoria.georgieva@cst-bg.net>
2023-05-23 09:18:41 +0300

    Add local front-end instructions

--- scripts/start-local-vuejs-client.sh
+++ scripts/start-local-vuejs-client.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
-sudo git clone https://github.com/Daiteap/daiteap-ui.git /workspaces/daiteap-ui
-sudo chmod -R 777 /workspaces/daiteap-ui/app
-cd /workspaces/daiteap-ui/app
+# Install Requirements - Only On First Run
+
+cd ./app
 sudo rm -r -f node_modules/
 sudo rm package-lock.json
-
-export VUE_APP_THEME=daiteap
-export VUE_APP_SINGLE_USER_MODE=False
-
 sudo apt install npm nginx -y
 sudo npm install -g @vue/cli
 sudo npm uninstall node-sass -g && npm cache clean --force && npm install node-sass
-sudo cp /workspaces/daiteap-ui/app/nginx/cloudcluster.conf /etc/nginx/sites-enabled/cloudcluster.conf
 npm install
 sudo service nginx restart
+export VUE_APP_THEME=daiteap
+export VUE_APP_SINGLE_USER_MODE=False
 npm run build -- --modern
 
-npm run serve -- --port 8082
+# Run UI
 
-# sudo apt install python3 python2 make g++
-# sudo apk add --update py-pip
-# pip3 install markdown mkdocs jinja2==3.0.3
-# cp ./public/favicon-$VUE_APP_THEME.ico ./docs/docs/img/favicon.ico
-# mkdocs build -f ./docs/mkdocs.yaml --site-dir ../public/documentation
+export VUE_APP_THEME=daiteap
+export VUE_APP_SINGLE_USER_MODE=False
+npm run serve -- --port 8084
+
+# Run Docs
+
+cp ./public/favicon-daiteap.ico ./docs/docs/img/favicon.ico
+mkdocs build -f ./docs/mkdocs.yaml --site-dir ../public/documentation
+mkdocs serve --config-file ./docs/mkdocs.yaml -a localhost:8085
