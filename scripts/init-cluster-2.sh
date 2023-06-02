96b5c438c55f0af02e3c0446175840d58b707419
Desislav Arashev <darashev@cst-bg.net>
2023-06-02 13:40:17 +0300

    Fix init-cluster-2.sh minor problem

--- scripts/init-cluster-2.sh
+++ scripts/init-cluster-2.sh
@@ -14,7 +14,7 @@ kubectl -n daiteap rollout status deploy platform-api --timeout=15m
 echo --- Copy Static Files ---
 
 export NFS_POD=$(kubectl -n daiteap get pods --no-headers -o custom-columns=":metadata.name" | grep nfs-server)
-kubectl -n daiteap wait --timeout=15m --for=condition=ready pod $NFS_POD
+kubectl -n daiteap wait --timeout=15m --for=condition=ready pod "$NFS_POD"
 
 kubectl -n daiteap cp docker-compose/service_logo $NFS_POD:/exports/service_logo
 kubectl -n daiteap cp docker-compose/drf-yasg $NFS_POD:/exports/drf-yasg
@@ -23,7 +23,7 @@ echo --- Execute Database Migrations ---
 
 echo ---- Waiting For Platform Pods ----
 export BACKEND_POD=$(kubectl get pods -n daiteap -l app=platform-api -o json | jq -r '.items[] | select(.status.phase!="Succeeded" and .status.phase!="Failed") | .metadata.name' | head -n 1)
-kubectl -n daiteap wait --timeout=15m --for=condition=ready pod $BACKEND_POD
+kubectl -n daiteap wait --timeout=15m --for=condition=ready pod "$BACKEND_POD"
 
 kubectl -n daiteap exec -it $BACKEND_POD -- /bin/sh -c "python3 ./manage.py migrate"
 kubectl -n daiteap exec -it $BACKEND_POD -- /bin/sh -c "python3 ./manage.py fix_service_catalog_prod"
@@ -33,8 +33,6 @@ echo --- Restart Back-End Pods ---
 kubectl -n daiteap rollout restart deploy platform-api
 kubectl -n daiteap rollout status deploy platform-api --timeout=15m
 echo ---- Waiting For Platform Pods ----
-export BACKEND_POD=$(kubectl get pods -n daiteap -l app=platform-api -o json | jq -r '.items[] | select(.status.phase!="Succeeded" and .status.phase!="Failed") | .metadata.name' | head -n 1)
-kubectl -n daiteap wait --timeout=15m --for=condition=ready pod $BACKEND_POD
 
 echo --- Port-Forward UI ---
 
